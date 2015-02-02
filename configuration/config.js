7c0e3c30de61a2ea128fd0e5aa42c908638228cb
ParinModi2 <modi.parin@gmail.com>
2015-02-02 14:13:25 -0800

    Update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -4,8 +4,8 @@ module.exports={
 * It consists of Twitter app information, database information.
 */
 
-	"facebook_api_key" : "1417182868574339",
-	"facebook_api_secret"	:"a603e86db522532136ba437e55fbbcb1",
+	"facebook_api_key" : "XXXX",
+	"facebook_api_secret"	:"ZZZZ",
 	"callback_url":"http://localhost:3000/auth/facebook/callback",
 	"database":'true'
 }
